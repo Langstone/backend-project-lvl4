97ba3857e78a5ed269699ff0fc87c855d483156f
Langstone <kalipsomart@gmail.com>
2022-07-25 14:45:26 +0600

    refactoring src/index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.sidebars.css';
-import 'bootstrap/dist/js/bootstrap.sidebars.js';
+import '../sidebars_style/sidebars.css';
+import '../sidebars_style/sidebars.js';

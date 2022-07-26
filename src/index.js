43bf0b489933d8c17fd00e3567085192314d7f53
Langstone <kalipsomart@gmail.com>
2022-07-26 20:44:29 +0600

    delete sidebars style

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../sidebars_style/sidebars.css';
-import '../sidebars_style/sidebars.js';

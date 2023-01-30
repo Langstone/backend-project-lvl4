09298b8e7fd7eeba3e3fcf2405665f4b78427b17
Langstone <kalipsomart@gmail.com>
2023-01-30 18:29:27 +0600

    delete TasksLabels.cjs from server/index.cjs

--- server/models/index.cjs
+++ server/models/index.cjs
@@ -2,7 +2,6 @@ const User = require('./User.cjs');
 const TaskStatus = require('./TaskStatus.cjs');
 const Task = require('./Task.cjs');
 const Label = require('./Label.cjs');
-const TasksLabels = require('./TasksLabels.cjs');
 
 module.exports = [
   User,
